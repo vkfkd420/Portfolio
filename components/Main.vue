20b20ba8fd5f6d8542b7c0b3b2e180c605860b4d
vkfkd420 <80494509+vkfkd420@users.noreply.github.com>
2024-06-24 16:49:52 +0900

    first
    first

---
+++ components/Main.vue
@@ -0,0 +1,288 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <body class="is-preload">
+      <!-- Nav -->
+      <nav id="nav">
+        <div class="fav">
+          <div>
+            <img src="~/assets/css/images/fav.png" alt="" />
+          </div>
+          <div>
+            <span>KHM's Portfolio</span>
+          </div>
+        </div>
+        <ul class="container">
+          <li><a href="#home">HOME</a></li>
+          <li><a href="#work">ABOUT ME</a></li>
+          <li><a href="#skills">SKILLS</a></li>
+          <li><a href="#portfolio">PORTFOLIO</a></li>
+          <li><a href="#contact">CONTACT</a></li>
+        </ul>
+      </nav>
+
+      <!-- Home -->
+      <article id="home" class="wrapper style1">
+        <div class="container">
+          <div class="row">
+            <div class="col-4 col-5-large col-12-medium">
+              <span class="image fit"><img src="~/assets/images/pic00.jpg" alt="" /></span>
+            </div>
+            <div class="col-8 col-7-large col-12-medium">
+              <header>
+                <h1>
+                  Make it happen Websites,<br />
+                  Solution, Platform
+                </h1>
+              </header>
+              <p>
+                다양한 환경과 개발 언어로 맞춤형 웹사이트를 구축합니다. 노하우가 담긴 프로젝트
+                작업물을 살펴보세요. test
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <!-- Work -->
+      <article id="work" class="wrapper style2">
+        <div class="container">
+          <header>
+            <h2><strong>About Me</strong></h2>
+          </header>
+          <div class="row aln-center">
+            <div class="col-4 col-6-medium col-12-small">
+              <section class="box style1">
+                <img src="~/assets/css/images/icon/profile.png" alt="profile" />
+                <div class="inner">
+                  <h3>이름</h3>
+                  <p>강현민</p>
+                </div>
+              </section>
+            </div>
+            <div class="col-4 col-6-medium col-12-small">
+              <section class="box style1">
+                <img src="~/assets/css/images/icon/calendar.png" alt="calendar" />
+                <div class="inner">
+                  <h3>생년월일</h3>
+                  <p>94.04.20</p>
+                </div>
+              </section>
+            </div>
+            <div class="col-4 col-6-medium col-12-small">
+              <section class="box style1">
+                <img src="~/assets/css/images/icon/home.png" alt="home" />
+                <div class="inner">
+                  <h3>주소지</h3>
+                  <p>서울특별시 영등포구</p>
+                </div>
+              </section>
+            </div>
+          </div>
+          <div class="row aln-center">
+            <div class="col-4 col-6-medium col-12-small">
+              <section class="box style1">
+                <img src="~/assets/css/images/icon/tel.png" alt="tel" />
+                <div class="inner">
+                  <h3>연락처</h3>
+                  <p>010-2982-9609</p>
+                </div>
+              </section>
+            </div>
+            <div class="col-4 col-6-medium col-12-small">
+              <section class="box style1">
+                <img src="~/assets/css/images/icon/email.png" alt="email" />
+                <div class="inner">
+                  <h3>이메일</h3>
+                  <p>vkfkd420@naver.com</p>
+                </div>
+              </section>
+            </div>
+            <div class="col-4 col-6-medium col-12-small">
+              <section class="box style1">
+                <img src="~/assets/css/images/icon/education.png" alt="education" />
+                <div class="inner">
+                  <h3>학력</h3>
+                  <p>서일대학교(정보통신학과)</p>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <!-- Portfolio -->
+      <article id="portfolio" class="wrapper style3">
+        <div class="container">
+          <header>
+            <h2>Here’s some stuff I made recently.</h2>
+            <p>Proin odio consequat sapien vestibulum consequat lorem dolore feugiat.</p>
+          </header>
+          <div class="row">
+            <div class="col-4 col-6-medium col-12-small">
+              <article class="box style2">
+                <a href="#" class="image featured"
+                  ><img src="~/assets/images/pic01.jpg" alt=""
+                /></a>
+                <h3><a href="#">Magna feugiat</a></h3>
+                <p>Ornare nulla proin odio consequat.</p>
+              </article>
+            </div>
+            <div class="col-4 col-6-medium col-12-small">
+              <article class="box style2">
+                <a href="#" class="image featured"
+                  ><img src="~/assets/images/pic02.jpg" alt=""
+                /></a>
+                <h3><a href="#">Veroeros primis</a></h3>
+                <p>Ornare nulla proin odio consequat.</p>
+              </article>
+            </div>
+            <div class="col-4 col-6-medium col-12-small">
+              <article class="box style2">
+                <a href="#" class="image featured"
+                  ><img src="~/assets/images/pic03.jpg" alt=""
+                /></a>
+                <h3><a href="#">Lorem ipsum</a></h3>
+                <p>Ornare nulla proin odio consequat.</p>
+              </article>
+            </div>
+            <div class="col-4 col-6-medium col-12-small">
+              <article class="box style2">
+                <a href="#" class="image featured"
+                  ><img src="~/assets/images/pic04.jpg" alt=""
+                /></a>
+                <h3><a href="#">Tempus dolore</a></h3>
+                <p>Ornare nulla proin odio consequat.</p>
+              </article>
+            </div>
+            <div class="col-4 col-6-medium col-12-small">
+              <article class="box style2">
+                <a href="#" class="image featured"
+                  ><img src="~/assets/images/pic05.jpg" alt=""
+                /></a>
+                <h3><a href="#">Feugiat aliquam</a></h3>
+                <p>Ornare nulla proin odio consequat.</p>
+              </article>
+            </div>
+            <div class="col-4 col-6-medium col-12-small">
+              <article class="box style2">
+                <a href="#" class="image featured"
+                  ><img src="~/assets/images/pic06.jpg" alt=""
+                /></a>
+                <h3><a href="#">Sed amet ornare</a></h3>
+                <p>Ornare nulla proin odio consequat.</p>
+              </article>
+            </div>
+          </div>
+          <footer>
+            <p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
+            <a href="#contact" class="button large scrolly">Get in touch with me</a>
+          </footer>
+        </div>
+      </article>
+
+      <!-- Contact -->
+      <article id="contact" class="wrapper style4">
+        <div class="container medium">
+          <header>
+            <h2>Have me make stuff for you.</h2>
+            <p>Ornare nulla proin odio consequat sapien vestibulum ipsum.</p>
+          </header>
+          <div class="row">
+            <div class="col-12">
+              <form method="post" action="#">
+                <div class="row">
+                  <div class="col-6 col-12-small">
+                    <input type="text" name="name" id="name" placeholder="Name" />
+                  </div>
+                  <div class="col-6 col-12-small">
+                    <input type="text" name="email" id="email" placeholder="Email" />
+                  </div>
+                  <div class="col-12">
+                    <input type="text" name="subject" id="subject" placeholder="Subject" />
+                  </div>
+                  <div class="col-12">
+                    <textarea name="message" id="message" placeholder="Message"></textarea>
+                  </div>
+                  <div class="col-12">
+                    <ul class="actions">
+                      <li><input type="submit" value="Send Message" /></li>
+                      <li><input type="reset" value="Clear Form" class="alt" /></li>
+                    </ul>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div class="col-12">
+              <hr />
+              <h3>Find me on ...</h3>
+              <ul class="social">
+                <li>
+                  <a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a>
+                </li>
+                <li>
+                  <a href="#" class="icon brands fa-facebook-f"
+                    ><span class="label">Facebook</span></a
+                  >
+                </li>
+                <li>
+                  <a href="#" class="icon brands fa-dribbble"
+                    ><span class="label">Dribbble</span></a
+                  >
+                </li>
+                <li>
+                  <a href="#" class="icon brands fa-linkedin-in"
+                    ><span class="label">LinkedIn</span></a
+                  >
+                </li>
+                <li>
+                  <a href="#" class="icon brands fa-tumblr"><span class="label">Tumblr</span></a>
+                </li>
+                <li>
+                  <a href="#" class="icon brands fa-google-plus"
+                    ><span class="label">Google+</span></a
+                  >
+                </li>
+                <li>
+                  <a href="#" class="icon brands fa-github"><span class="label">Github</span></a>
+                </li>
+                <!--
+            <li><a href="#" class="icon solid fa-rss"><span>RSS</span></a></li>
+            <li><a href="#" class="icon brands fa-instagram"><span>Instagram</span></a></li>
+            <li><a href="#" class="icon brands fa-foursquare"><span>Foursquare</span></a></li>
+            <li><a href="#" class="icon brands fa-skype"><span>Skype</span></a></li>
+            <li><a href="#" class="icon brands fa-soundcloud"><span>Soundcloud</span></a></li>
+            <li><a href="#" class="icon brands fa-youtube"><span>YouTube</span></a></li>
+            <li><a href="#" class="icon brands fa-blogger"><span>Blogger</span></a></li>
+            <li><a href="#" class="icon brands fa-flickr"><span>Flickr</span></a></li>
+            <li><a href="#" class="icon brands fa-vimeo"><span>Vimeo</span></a></li>
+            -->
+              </ul>
+              <hr />
+            </div>
+          </div>
+          <footer>
+            <ul id="copyright">
+              <li>&copy; Untitled. All rights reserved.</li>
+              <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            </ul>
+          </footer>
+        </div>
+      </article>
+
+      <!-- Scripts -->
+      <script src="assets/js/jquery.min.js"></script>
+      <script src="assets/js/jquery.scrolly.min.js"></script>
+      <script src="assets/js/browser.min.js"></script>
+      <script src="assets/js/breakpoints.min.js"></script>
+      <script src="assets/js/util.js"></script>
+      <script src="assets/js/main.js"></script>
+    </body>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,55 @@
+export default {
+  target: 'static', // 정적 웹사이트임을 명시
+  router: {
+    base: '/Portfolio/' // github repository 이름 넣기
+  },
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'portfolio',
+    htmlAttrs: {
+      lang: 'ko'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: '~/assets/js/jquery.min.js', defer: true },
+      { src: '~/assets/js/jquery.scrolly.min.js', defer: true },
+      { src: '~/assets/js/browser.min.js', defer: true },
+      { src: '~/assets/js/breakpoints.min.js', defer: true },
+      { src: '~/assets/js/util.js', defer: true },
+      { src: '~/assets/js/main.js', defer: true }
+      // 추가적인 스크립트를 필요에 따라 설정
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+     '~/assets/css/main.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: false,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
